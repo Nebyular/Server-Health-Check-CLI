5d89febcc6210791f0aff622495d50e6dadab25d
Jack Ford <Jack Ford>
2017-07-21 14:12:34 +1000

    Add project files.

---
+++ HealthChkTool/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.NetworkInformation;
+using System.Net.Mail;
+
+namespace HealthChkTool
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string targetIp = "8.8.8.8";
+            string icmpRslts;
+
+            Ping PingSender = new Ping();
+            PingReply reply = PingSender.Send(targetIp);
+        }
+    }
+
+    public class Smtp
+    {
+
+    }
+}
